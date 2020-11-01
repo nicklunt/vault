224963df7315222f5a1383a3dee3ac018f5a533a
nicklunt <nlunt1970@gmail.com>
2020-11-01 16:37:32 +0000

    added jenkinsfile

--- profile.tf
+++ profile.tf
@@ -1,24 +1,40 @@
-  "Statement": [
-    {
-      "Action": [
-        "dynamodb:DescribeLimits",
-        "dynamodb:DescribeTimeToLive",
-        "dynamodb:ListTagsOfResource",
-        "dynamodb:DescribeReservedCapacityOfferings",
-        "dynamodb:DescribeReservedCapacity",
-        "dynamodb:ListTables",
-        "dynamodb:BatchGetItem",
-        "dynamodb:BatchWriteItem",
-        "dynamodb:CreateTable",
-        "dynamodb:DeleteItem",
-        "dynamodb:GetItem",
-        "dynamodb:GetRecords",
-        "dynamodb:PutItem",
-        "dynamodb:Query",
-        "dynamodb:UpdateItem",
-        "dynamodb:Scan",
-        "dynamodb:DescribeTable"
-      ],
-      "Effect": "Allow",
-      "Resource": [ "arn:aws:dynamodb:us-east-1:... dynamodb table ARN" ]
-    },
+  resource "aws_iam_instance_profile" "vault-profile" {
+    name = "vault-profile"
+    role = aws_iam_role.vault-role.name
+  }
+
+  resource "aws_iam_role" "vault-role" {
+    name = "vault-role"
+    path = "/"
+
+    assume_role_policy = <<EOF
+  {
+    "Version": "2012-10-17",
+        "Statement": [
+            {
+            "Action": [
+                "dynamodb:DescribeLimits",
+                "dynamodb:DescribeTimeToLive",
+                "dynamodb:ListTagsOfResource",
+                "dynamodb:DescribeReservedCapacityOfferings",
+                "dynamodb:DescribeReservedCapacity",
+                "dynamodb:ListTables",
+                "dynamodb:BatchGetItem",
+                "dynamodb:BatchWriteItem",
+                "dynamodb:CreateTable",
+                "dynamodb:DeleteItem",
+                "dynamodb:GetItem",
+                "dynamodb:GetRecords",
+                "dynamodb:PutItem",
+                "dynamodb:Query",
+                "dynamodb:UpdateItem",
+                "dynamodb:Scan",
+                "dynamodb:DescribeTable"
+            ],
+            "Effect": "Allow",
+            "Sid": "Vault"
+            }
+        ]
+    }
+EOF
+}
