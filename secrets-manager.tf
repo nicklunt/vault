c45792e60584c3630fec64f586f54113e006978c
nicklunt <nlunt1970@gmail.com>
2021-05-22 21:20:03 +0100

    comments

--- secrets-manager.tf
+++ secrets-manager.tf
@@ -2,10 +2,10 @@
 resource "aws_secretsmanager_secret" "vault-root-token" {
   name                    = var.vault-secret-name
   description             = "Vault root token"
+  # recovery set to 0 so we can recreate the secret as required for testing.
   recovery_window_in_days = 0
   tags = {
     Name = var.vault-secret-name
-
   }
 }
 
